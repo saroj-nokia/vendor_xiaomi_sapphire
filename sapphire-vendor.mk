1f1d490a6d7094317459f50d704d3f887ac79ac8
Saroj-Tajpuriya <80759612+saroj-nokia@users.noreply.github.com>
2025-06-29 20:47:01 +0545

    sapphire: update HotwordEnrollmentYGoogleHEXAGON_WIDEBAND.apk

--- sapphire-vendor.mk
+++ sapphire-vendor.mk
@@ -1666,7 +1666,7 @@ PRODUCT_PACKAGES += \
     TrustZoneAccessService \
     TxPwrAdmin \
     uimgbaservice \
-    HotwordEnrollmentOKGoogleHEXAGON_WIDEBAND \
+    HotwordEnrollmentYGoogleHEXAGON_WIDEBAND \
     HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
     AtFwd2 \
     DeviceInfo \
